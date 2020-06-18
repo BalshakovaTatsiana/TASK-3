be1835c5c48deee300ccffc85c8b48efb54f28fe
Tatsiana Balshakova <balshakovatatsiana@gmail.com>
2020-06-18 14:22:53 +0300

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int value = rand.Next(0, 99);
+            Console.WriteLine(value);
+            int value1 = rand.Next(0, 99);
+            Console.WriteLine(value1);
+            int value2 = rand.Next(0, 99);
+            Console.WriteLine(value2);
+            int value2 = rand.Next(0, 99);
+            Console.WriteLine(value2);
+
+
+        }
+    }
+}
